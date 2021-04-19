94b68caf9e6672714a9cb42124972452fa15d6e5
Benzino <benasabir@gmail.com>
2021-04-19 19:45:41 -0400

    updated app structure

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
-const pizzaRoutes = require('./pizza-routes');
 const commentRoutes = require('./comment-routes');
+const pizzaRoutes = require('./pizza-routes');
 
-// add prefix of `/pizzas, /comments` to routes created in `pizza-routes.js/comment-routes.js`
-router.use('/pizzas', pizzaRoutes);
 router.use('/comments', commentRoutes);
+router.use('/pizzas', pizzaRoutes);
 
 module.exports = router;
